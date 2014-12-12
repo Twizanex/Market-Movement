4dd534f54cc8f010dd59b726f861134887b05f8d
colemantung <cmtkusc@gmail.com>
2014-12-11 21:06:09 -0800

    Add Modal.

--- app/views/page/login.blade.php
+++ app/views/page/login.blade.php
@@ -58,6 +58,29 @@ License: You must have a valid license purchased only from themeforest(the above
 <div class="menu-toggler sidebar-toggler">
 </div>
 <!-- END SIDEBAR TOGGLER BUTTON -->
+
+
+<div class="modal fade bs-modal-lg" id="assignment" tabindex="-1" role="dialog" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+				<h4 class="modal-title">Modal Title</h4>
+			</div>
+			<div class="modal-body">
+				Modal body goes here
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn default" data-dismiss="modal">Close</button>
+				<button type="button" class="btn blue">Save changes</button>
+			</div>
+		</div>
+		<!-- /.modal-content -->
+	</div>
+	<!-- /.modal-dialog -->
+</div>
+
+
 <!-- BEGIN LOGIN -->
 <div class="content">
 	<!-- BEGIN LOGIN FORM -->
@@ -527,6 +550,13 @@ jQuery(document).ready(function() {
     );
 });
 </script>
+
+<script type="text/javascript">
+    $(window).load(function(){
+        $('#assignment').modal('show');
+    });
+</script>
+
 <!-- END JAVASCRIPTS -->
 </body>
 <!-- END BODY -->
